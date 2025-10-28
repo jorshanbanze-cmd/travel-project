073fac9872a6a99ad181daada32a258072cef156
Hamdan <hamdanmansoor5490@gmail.com>
2025-10-28 19:43:20 +0500

    Add initial React + Vite project

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{ padding: "80px", textAlign: "center" }}>
+      <h2>About</h2>
+      <p>About Karwane Voice of Aima...</p>
+    </div>
+  );
+}
+
+export default About; // ✅ Default export
